4420102065df0f83ba9537a80b6528f294b87b5b
Krishna Iyer <krishna.vadakkeputhanmadomsubramanian@uni-goettingen.de>
2026-01-12 10:51:24 +0100

    Add fix for poisson-like reversal

---
+++ src/SELF-PROPELLED-FILAMENT/fix_reversal.cpp
@@ -0,0 +1,81 @@
+#include "fix_reversal.h"
+
+#include "atom.h"
+#include "error.h"
+#include "update.h"
+#include <Random123/uniform.hpp>
+
+using namespace LAMMPS_NS;
+
+FixReversal::FixReversal(LAMMPS *lmp, int narg, char **arg) : Fix(lmp, narg, arg)
+{
+  if (narg < 4) utils::missing_cmd_args(FLERR, "fix reversal", error); 
+
+  nevery = utils::inumeric(FLERR, arg[3], false, lmp);
+  if (nevery < 0) {
+    error->all(FLERR, "Illegal Fix reversal ", nevery);
+  }
+
+  int iarg = 5;
+  if (iarg <= narg) {
+    avg_runtime = utils::numeric(FLERR, arg[4], false, lmp);
+    uk[0] = utils::numeric(FLERR, arg[5], false, lmp);
+  } else  {
+    error->all(FLERR, "Illegal Fix reversal command");
+  }
+
+  if (avg_runtime < 0.0) {
+    error->all(FLERR, "Illegal average runtime for Fix reversal");
+  }
+  rate = update->dt / avg_runtime;
+
+}
+
+FixReversal::~FixReversal()
+{
+  // destructor
+}
+
+int FixReversal::setmask()
+{
+  int mask = 0;
+  mask |= FixConst::END_OF_STEP;
+  return mask;
+}
+
+void FixReversal::init()
+{
+  if (atom->molecule_flag == 0) error->all(FLERR, "Fix reversal requires molecule IDs");
+}
+
+void FixReversal::end_of_step()
+{
+  int* mask = atom->mask;
+  tagint* mol = atom->molecule;
+  const int nlocal = atom->nlocal;
+
+
+  int reversal_flag;
+  int reversal_type;
+  int reversal_index = atom->find_custom("reversal", reversal_flag, reversal_type);
+  // reversal vector needs to exist for fix to work
+  if (reversal_index == -1) error->all(FLERR, "Fix reversal needs custom reversal variable");
+
+  int* reversal = atom->ivector[reversal_index];
+
+  // flip value of reversal for all atoms in group
+  for (int i = 0; i < nlocal; i++)
+  {
+    if (mask[i] & groupbit) 
+    { 
+      RNG::ctr_type c = {{0}};
+      c[0] = update->ntimestep;
+      RNG::key_type k = uk;
+      k[0] += mol[i];
+      auto r = rng(c,k);
+      if (r123::u01<double>(r.v[0]) <= rate) {
+        reversal[i] = !reversal[i];
+      }
+    }
+  }
+}
---
+++ src/SELF-PROPELLED-FILAMENT/fix_reversal.h
@@ -0,0 +1,34 @@
+#ifdef FIX_CLASS
+// clang-format off
+FixStyle(reversal,FixReversal);
+// clang-format on
+#else
+
+#ifndef LMP_FIX_REVERSAL_H
+#define LMP_FIX_REVERSAL_H
+
+#include "fix.h"
+
+#include <Random123/philox.h>
+
+typedef r123::Philox2x64 RNG;
+
+namespace LAMMPS_NS {
+
+class FixReversal : public Fix {
+ public:
+  FixReversal(class LAMMPS *, int, char **);
+  ~FixReversal() override;
+  void init() override;
+  int setmask() override;
+  void end_of_step() override;
+ protected:
+  double avg_runtime;
+  double rate;
+  RNG rng;
+  RNG::ukey_type uk = {{43923764583}};
+};
+}
+
+#endif
+#endif
